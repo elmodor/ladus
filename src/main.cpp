6e950c9b7893924bf632a589002adbaf1890104a
twobit <s8572327@gmail.com>
2016-09-06 20:36:38 +0200

    debug drawer

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,107 @@
 rmw::Context ctx;
 
 
+class DebugDrawer : public btIDebugDraw {
+public:
+
+	struct Vertex {
+		glm::vec3 pos;
+		glm::vec3 color;
+	};
+
+	void init() {
+
+		shader = ctx.create_shader(
+			VERTEX_SHADER(
+				layout(location = 0) in vec3 in_pos;
+				layout(location = 1) in vec3 in_color;
+				uniform mat4 u_mvp;
+				out vec3 v_color;
+				void main() {
+					gl_Position = u_mvp * vec4(in_pos, 1.0);
+					v_color = in_color;
+				}
+			), FRAGMENT_SHADER(
+				in vec3 v_color;
+				out vec4 out_color;
+				void main() {
+					out_color = vec4(v_color, 1.0);
+				}
+			));
+
+
+		vertex_buffer = ctx.create_vertex_buffer(rmw::BufferHint::StreamDraw);
+		vertex_array = ctx.create_vertex_array();
+		vertex_array->set_primitive_type(rmw::PrimitiveType::Lines);
+		vertex_array->set_attribute(0, *vertex_buffer, rmw::ComponentType::Float, 3, false,  0, sizeof(Model::Vertex));
+		vertex_array->set_attribute(1, *vertex_buffer, rmw::ComponentType::Float, 3, false, 12, sizeof(Model::Vertex));
+
+	}
+
+	void draw(rmw::RenderState rs, const glm::mat4& mvp) {
+
+		shader->set_uniform("u_mvp", mvp);
+
+		vertex_buffer->init_data(vertices);
+		vertex_array->set_count(vertices.size());
+		ctx.draw(rs, *shader, *vertex_array);
+
+		vertices.clear();
+	}
+
+
+	void drawLine(const btVector3& a, const btVector3& b, const btVector3& color) override {
+		vertices.push_back({
+			* (glm::vec3*) & a,
+			* (glm::vec3*) & color,
+		});
+		vertices.push_back({
+			* (glm::vec3*) & b,
+			* (glm::vec3*) & color,
+		});
+
+	}
+
+	void drawContactPoint (
+			const btVector3 &PointOnB,
+			const btVector3 &normalOnB,
+			btScalar distance,
+			int lifeTime,
+			const btVector3 &color)
+	{
+	}
+
+	void reportErrorWarning(const char* warningString) override {
+	}
+	void draw3dText(const btVector3& location, const char* textString) override {
+	}
+
+	void setDebugMode(int debugMode) override {
+		mode = debugMode;
+	}
+
+	int getDebugMode() const override {
+		return mode;
+	}
+
+
+private:
+
+	rmw::Shader::Ptr		shader;
+	rmw::VertexBuffer::Ptr	vertex_buffer;
+	rmw::VertexArray::Ptr	vertex_array;
+	std::vector<Vertex>		vertices;
+	int						mode;
+};
+
+
+
+
+
+
+
+
+
 class Scene {
 public:
 
@@ -71,14 +172,14 @@ public:
 
 		broadphase = std::make_unique<btDbvtBroadphase>();
 		solver = std::make_unique<btSequentialImpulseConstraintSolver>();
-		world = std::make_unique<btDiscreteDynamicsWorld>(
+		dynamics_world = std::make_unique<btDiscreteDynamicsWorld>(
 				dispatcher.get(),
 				broadphase.get(),
 				solver.get(),
 				collision_conf.get());
 
-		world->setGravity(btVector3(0, -10, 0));
-		world->setInternalTickCallback(Scene::update, static_cast<void*>(this), false);
+		dynamics_world->setGravity(btVector3(0, -10, 0));
+		dynamics_world->setInternalTickCallback(Scene::update, static_cast<void*>(this), false);
 
 
 
@@ -116,28 +217,37 @@ public:
 
 		rigid_body = std::make_unique<btRigidBody>(construction_info);
 
-		world->addRigidBody(rigid_body.get());
+		dynamics_world->addRigidBody(rigid_body.get());
 
+
+		// debug drawing
+		drawer.init();
+		drawer.setDebugMode(btIDebugDraw::DBG_DrawWireframe);
+		dynamics_world->setDebugDrawer(&drawer);
 	}
 
 	void update(float dt) {
-		world->stepSimulation(dt, 5);
+		dynamics_world->stepSimulation(dt, 5);
 	}
 
 
 
-	void draw(rmw::RenderState rs, const glm::vec2& camera) {
-
-		glm::mat4 mvp = glm::perspectiveFov<float>(1.0, 800, 600, 1, 100)
-					  * glm::translate(glm::vec3(camera.x, camera.y, -25));
+	void draw(rmw::RenderState rs, const glm::mat4& mvp) {
 
 		shader->set_uniform("u_mvp", mvp);
 
 		ctx.draw(rs, *shader, *vertex_array);
+
+
+		// debug drawing
+		dynamics_world->debugDrawWorld();
+		drawer.draw(rs, mvp);
 	}
 
 
 private:
+	DebugDrawer drawer;
+
 
 	void tick() {
 
@@ -146,8 +256,8 @@ private:
 	}
 
 
-	static void update(btDynamicsWorld *world, float dt) {
-		static_cast<Scene*>(world->getWorldUserInfo())->tick();
+	static void update(btDynamicsWorld* dynamics_world, float dt) {
+		static_cast<Scene*>(dynamics_world->getWorldUserInfo())->tick();
 	}
 
 
@@ -164,7 +274,7 @@ private:
 	std::unique_ptr<btCollisionDispatcher>					dispatcher;
 	std::unique_ptr<btDbvtBroadphase>						broadphase;
 	std::unique_ptr<btSequentialImpulseConstraintSolver>	solver;
-	std::unique_ptr<btDiscreteDynamicsWorld>				world;
+	std::unique_ptr<btDiscreteDynamicsWorld>				dynamics_world;
 
 
 	// ~per body stuff
@@ -255,7 +365,11 @@ int main(int argc, char** argv) {
 
 		ctx.clear(cs);
 
-		scene.draw(rs, camera);
+		glm::mat4 mvp = glm::perspectiveFov<float>(1.0, 800, 600, 1, 100)
+					  * glm::translate(glm::vec3(camera.x, camera.y, -25));
+
+		scene.draw(rs, mvp);
+
 
 		ctx.flip_buffers();
 	}
